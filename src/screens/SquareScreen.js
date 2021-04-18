2650b214d2165c146ff65efa4559fcf7a46195f1
ikyusix <ikyusixadam@gmail.com>
2021-04-18 21:08:38 +0700

    reusable color adjusters

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,9 +1,12 @@
 import React from "react";
 import {Text, View, StyleSheet} from "react-native";
+import ColorCounter from "../components/ColorCounter";
 
 const SquareScreen = () => {
     return <View>
-        <Text>Square screen</Text>
+        <ColorCounter color="Red"/>
+        <ColorCounter color="Blue"/>
+        <ColorCounter color="Green"/>
     </View>
 }
 
