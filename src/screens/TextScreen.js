068ab9f38bf7afda58346d19bbc182a57061a336
ikyusix <ikyusixadam@gmail.com>
2021-04-19 00:01:24 +0700

    add text screen

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Text, View, StyleSheet} from "react-native";
+
+const TextScreen = () => {
+    return <View>
+        <Text>Text screen</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({})
+
+export default TextScreen
