3fc603acedccab583070461fc2fe06427da15dae
ikyusix <ikyusixadam@gmail.com>
2021-04-18 23:17:32 +0700

    applying state with a reducer

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -22,22 +22,23 @@ const reducer = (state, action) => {
 
 const SquareScreen = () => {
     const [state, dispatch] = useReducer(reducer, {red: 0, green: 0, blue: 0})
+    const {red, green, blue} = state
 
     return <View>
         <ColorCounter
             color="Red"
-            onIncrease={() => }
-            onDecrease={() => }
+            onIncrease={() => dispatch({colorToChange: 'red', amount: COLOR_INCREMENT})}
+            onDecrease={() => dispatch({colorToChange: 'red', amount: -1 * COLOR_INCREMENT})}
         />
         <ColorCounter
             color="Green"
-            onIncrease={() => }
-            onDecrease={() => }
+            onIncrease={() => dispatch({colorToChange: 'green', amount: COLOR_INCREMENT})}
+            onDecrease={() => dispatch({colorToChange: 'green', amount: -1 * COLOR_INCREMENT})}
         />
         <ColorCounter
             color="Blue"
-            onIncrease={() => }
-            onDecrease={() => }
+            onIncrease={() => dispatch({colorToChange: 'blue', amount: COLOR_INCREMENT})}
+            onDecrease={() => dispatch({colorToChange: 'blue', amount: -1 * COLOR_INCREMENT})}
         />
         <View style={{height: 150, width: 150, backgroundColor: `rgb(${red}, ${green}, ${blue})`}}/>
     </View>
