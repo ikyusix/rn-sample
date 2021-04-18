0956a9e0019ccecde605554f8d973fbfd857e127
ikyusix <ikyusixadam@gmail.com>
2021-04-18 20:21:13 +0700

    generating random color

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -6,7 +6,16 @@ const ColorScreen = () => {
         <Button title="Add a color" onPress={() => {
 
         }}/>
+        <View style={{height: 100, width: 100, backgroundColor: randomRGB()}}/>
     </View>
+}
+
+const randomRGB = () => {
+    const red = Math.floor(Math.random() * 256)
+    const green = Math.floor(Math.random() * 256)
+    const blue = Math.floor(Math.random() * 256)
+
+    return `rgb(${red}, ${green}, ${blue})`
 }
 
 const styles = StyleSheet.create({})
